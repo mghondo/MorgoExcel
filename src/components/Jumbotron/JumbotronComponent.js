753ed376a8eacd2afe1b6e71ab87c817da1c2a60
Morgan Hondros <morganhondros@gmail.com>
2024-08-08 10:08:32 -0400

    BEFORE PUTTING PYTHON ON LAMBDA

--- src/components/Jumbotron/JumbotronComponent.js
+++ src/components/Jumbotron/JumbotronComponent.js
@@ -36,7 +36,7 @@ const JumbotronComponent = () => {
     <div className="jumbotron jumbotron-fluid jumbotron-custom">
       <div className="container-fluid text-center">
         <h1 className="display-4">Morgo Excel</h1>
-        <h4>Making the day faster...{showZach && "...except for Zach!!"}</h4>
+        <h4>Making the day faster.{showZach && ".....except for Zach!!"}</h4>
       </div>
     </div>
   );
