750dc43a6dbaab7e99a66c845890611deb2c2674
Saleh Usman <salehusman91@gmail.com>
2024-10-10 21:09:28 +0500

    updated port to listen from all hosts

--- backend/app.py
+++ backend/app.py
@@ -71,4 +71,4 @@ if __name__ == '__main__':
     os.makedirs(MORNING_COMPLETE_FOLDER, exist_ok=True)
     os.makedirs(WEEKLY_UPLOAD_FOLDER, exist_ok=True)
     os.makedirs(WEEKLY_COMPLETE_FOLDER, exist_ok=True)
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
